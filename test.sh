486718115e148f10339a1c7622172da302a0ab4b
David Nevarez <david.nevarez03@hotmail.com>
2023-01-25 20:17:19 -0700

    updated

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 for ((i=1; i<=4; i++)) do
-    echo "Testing case $i"
+    echo "Testing case $i" # prints which test case number it's currently testing 
+
+    # Compiles the java file and runs it with the input file and outputs the result to myout$i.txt(depends on which test case it's currently testing)
     java -cp bin/ $1 < test/input$i.txt > test/myout$i.txt
     diff test/myout$i.txt test/output$i.txt
 done
